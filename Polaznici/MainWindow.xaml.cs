0225f104195f561d34f7be549e07f7ed3a017526
Dragan Pepic <d.pepic@hotmail.com>
2021-10-06 10:08:58 +0200

    hghg

--- Polaznici/MainWindow.xaml.cs
+++ Polaznici/MainWindow.xaml.cs
@@ -24,17 +24,74 @@ namespace Polaznici
 		public ObservableCollection<Kurs> Kursevi { get; set; } = new();
 		public Kurs Kurs { get; set; } = new();
 
+		public ObservableCollection<Polaznik> Polaznici { get; set; } = new();
+		public Kurs Polaznik { get; set; } = new();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			Kursevi.Add(new Kurs { Naziv = "asd" });
+			Kursevi.Add(new Kurs { Naziv = "qwe" });
+			Kursevi.Add(new Kurs { Naziv = "zxc" });
+			Kursevi.Add(new Kurs { Naziv = "fgh" });
+
+			Polaznici.Add(new Polaznik { Ime = "Pera", Prezime = "Peric" });
+			Polaznici.Add(new Polaznik { Ime = "Neko", Prezime = "Nekic" });
 			dgKurs.ItemsSource = Kursevi;
 			spKurs.DataContext = Kurs;
+			dgPolaznici.ItemsSource = Polaznici;
+			spPolaznici.DataContext = Polaznik;
 		}
 
 		private void Unos(object sender, RoutedEventArgs e)
 		{
-			Kursevi.Add(spKurs.DataContext as Kurs);
-			spKurs.DataContext = new Kurs();
+			Kurs k = spKurs.DataContext as Kurs;
+			if (!(string.IsNullOrEmpty(k.Naziv) ||
+				string.IsNullOrWhiteSpace(k.Naziv)))
+			{
+				k.Naziv = k.Naziv.Trim();
+
+				//Pretraga iteracijom
+				//bool nadjenDuplikat = false;
+				//foreach (Kurs ku in Kursevi)
+				//{
+				//	if (ku.Naziv.ToLower() == k.Naziv.ToLower())
+				//	{
+				//		nadjenDuplikat = true;
+				//		break;
+				//	}
+				//}
+
+				//Provera sa LINQ-om :)
+				if (!Kursevi.Where(ku => ku.Naziv.ToLower() == k.Naziv.ToLower()).Any())
+				{
+					Kursevi.Add(spKurs.DataContext as Kurs);
+					spKurs.DataContext = new Kurs();
+				} else
+				{
+					MessageBox.Show("Jok!");
+				}
+
+				//Star koliko dinosaursi
+				//for (int indeks = 0; indeks < Kursevi.Count; indeks++)
+				//	MessageBox.Show(Kursevi[indeks].Naziv);
+
+				//Moderniji nacin
+				//foreach (Kurs ku in Kursevi)
+				//	MessageBox.Show(ku.Naziv);
+
+				//Ultra moderan nacin
+				//Kursevi.ToList().ForEach(k => MessageBox.Show(k.Naziv));
+
+				
+			}
+			else
+				MessageBox.Show("Jok!");
+		}
+
+		private void Brisanje(object sender, RoutedEventArgs e)
+		{
+			Kursevi.Remove((dgKurs.SelectedItem as Kurs));
 		}
 	}
 
